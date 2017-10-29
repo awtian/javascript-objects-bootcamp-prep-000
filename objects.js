fca67167398daffcc70e0f8867a2e28274e5d522
Awtian Akbar <me@awtian.com>
2017-10-29 11:48:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,8 @@ var playlist = {
   logic: "1-800"
 };
 
-function updatePlaylist (playlist, artist, songTitle) {
-  playlist.artist = songTitle
+function updatePlaylist (playlist, artist, song) {
+  playlist.artist = song
   return playlist
 }
 

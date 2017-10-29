5fe59b6c6c59efcfbec5c8715b0203686800379a
Awtian Akbar <me@awtian.com>
2017-10-29 11:57:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,3 +11,6 @@ function removeFromPlaylist (playlist, artist) {
   delete playlist.artist
   return playlist
 }
+
+
+console.log ((updatePlaylist({}, 'Phil Ochs', "Here's to the State of Mississippi")).)

cc827168eb2d6167f9bd7356a3e2b141977fa880
Awtian Akbar <me@awtian.com>
2017-10-29 11:50:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   keyaki: "Futari Saison"
 };
 
-function updatePlaylist (playlist, artist, song) {
+function updatePlaylist (playlist, `artist`, `song`) {
   playlist.artist = song
   return playlist
 }

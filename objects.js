f23294d035ce3b5d2e5d5c35faa63711e74a6932
Awtian Akbar <me@awtian.com>
2017-10-29 12:19:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist (playlist, artistName, song) {
 }
 
 function removeFromPlaylist (playlist, artist) {
-  delete playlist.object.artist
+  var fuck = `${playlist}.${artist}`
   return playlist
 }
 
